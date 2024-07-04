9713542010f464fa9ac7b0dc89804bde91011685
woolit <genius8111@naver.com>
2024-07-05 02:11:05 +0900

    fix: 게시판 리스트, 상세 페이지 생성 fix: 게시글 삭제, 수정 기능 구현

---
+++ src/main/java/com/study/board/service/BoardService.java
@@ -0,0 +1,40 @@
+package com.study.board.service;
+
+import com.study.board.entity.Board;
+import com.study.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    //글 작성 처리
+    public void write(Board board) {
+
+        boardRepository.save(board);
+    }
+
+    //게시글 리스트 처리
+    public List<Board> boardList() {
+
+        return boardRepository.findAll();
+    }
+
+    //특정 게시글 불러오기
+    public Board boardView(Integer id) {
+
+        return boardRepository.findById(id).get();
+    }
+
+    //특정 게시글 삭제
+    public void boardDelete(Integer id) {
+
+        boardRepository.deleteById(id);
+    }
+
+}
